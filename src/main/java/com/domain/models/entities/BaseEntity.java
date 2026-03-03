0c60bc81870eeaaa7ecdf0d79a6cafacfd8950cf
Agungpurr <agungpur1102@gmail.com>
2026-03-03 18:05:48 +0700

    auditing with spring JPA

---
+++ src/main/java/com/domain/models/entities/BaseEntity.java
@@ -0,0 +1,66 @@
+package com.domain.models.entities;
+
+import java.util.Date;
+
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import jakarta.persistence.Temporal;
+import jakarta.persistence.TemporalType;
+
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public class BaseEntity<T> {
+    
+    @CreatedBy
+    protected T createdBy;
+
+    @CreatedDate
+    @Temporal(TemporalType.TIMESTAMP)
+    protected Date creatDate;
+
+    @LastModifiedBy
+    protected T updatedBy;
+
+    @LastModifiedDate
+    @Temporal(TemporalType.TIMESTAMP)
+    protected Date updatedDate;
+
+    public T getCreatedBy() {
+        return createdBy;
+    }
+
+    public void setCreatedBy(T createdBy) {
+        this.createdBy = createdBy;
+    }
+
+    public Date getCreatDate() {
+        return creatDate;
+    }
+
+    public void setCreatDate(Date creatDate) {
+        this.creatDate = creatDate;
+    }
+
+    public T getUpdatedBy() {
+        return updatedBy;
+    }
+
+    public void setUpdatedBy(T updatedBy) {
+        this.updatedBy = updatedBy;
+    }
+
+    public Date getUpdatedDate() {
+        return updatedDate;
+    }
+
+    public void setUpdatedDate(Date updatedDate) {
+        this.updatedDate = updatedDate;
+    }
+    
+}
